6de6515c6c864d7d3a1a14d1e24230161916e187
Ali Rachidi <ali.rachidi73@gmail.com>
2026-06-09 11:05:53 -0400

    Allow time_to_take='all' to extract whole signal

--- src/alpss/io/reading.py
+++ src/alpss/io/reading.py
@@ -8,20 +8,27 @@ logger = logging.getLogger("alpss")
 def extract_data(inputs):
     t_step = 1 / inputs["sample_rate"]
     rows_to_skip = inputs["header_lines"] + inputs["time_to_skip"] / t_step
-    nrows = inputs["time_to_take"] / t_step
+
+    # Allow time_to_take to be "all" to read the entire signal
+    time_to_take = inputs["time_to_take"]
+    if isinstance(time_to_take, str) and time_to_take.lower() == "all":
+        nrows = None
+    else:
+        nrows = int(time_to_take / t_step)
+
     fname = inputs["filepath"]
 
     if "bytestring" in inputs and isinstance(inputs["bytestring"], bytes):
         data = pd.read_csv(
             io.BytesIO(inputs["bytestring"]),
             skiprows=int(rows_to_skip),
-            nrows=int(nrows),
+            nrows=nrows,
         )
     elif isinstance(fname, str):
         data = pd.read_csv(
             fname,
             skiprows=int(rows_to_skip),
-            nrows=int(nrows),
+            nrows=nrows,
         )
     else:
         raise TypeError(
